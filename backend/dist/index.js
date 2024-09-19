907556587bc63fa18edc13c13ccd73aee33dbd84
saju03 <sajusuresh0752@gmail.com>
2024-09-20 01:39:50 +0530

    react build and server build setup complete

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from 'dotenv';
 import initDB from "./Database/databaseConfig.js";
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 initDB();
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 3000;
-app.all('/', (req, res) => {
-    res.send('hello world');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.listen(PORT, () => console.log(`server started http://localhost:${PORT}`));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,24 @@
-import express, { Application, Express } from "express";
+import express, { Application, Express, Request, Response } from "express";
 import dotenv from 'dotenv';
 import initDB from "./Database/databaseConfig.js";
+import path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename:string = fileURLToPath(import.meta.url);
+const __dirname:string = path.dirname(__filename);
 initDB()
 dotenv.config();
 const app:Application = express()
 
+
+
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT:string | number  = process.env.PORT || 3000
 
-
-app.all('/',(req,res)=>{
-    res.send('hello world')
-})
+app.get('*', (req:Request, res:Response) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
 app.listen(PORT,()=>console.log(`server started http://localhost:${PORT}`))
